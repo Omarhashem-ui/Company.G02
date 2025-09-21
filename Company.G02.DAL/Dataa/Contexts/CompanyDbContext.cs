21d03ecca634100144412a2dc201002e292496e2
omaarhaashem@gmail.com <omaarhaashem@gmail.com>
2025-09-21 16:14:28 +0300

    Implemantation Of DAL

---
+++ Company.G02.DAL/Dataa/Contexts/CompanyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Dataa.Contexts
+{
+    internal class CompanyDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.; Database=CompanyG02; TrustedConnection=True; TrustServerCertificate=True");
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
