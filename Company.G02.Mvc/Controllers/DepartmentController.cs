611637466e0cfcf28e9c7f3a45f14d7cee683dda
omaarhaashem@gmail.com <omaarhaashem@gmail.com>
2025-09-24 11:24:14 +0300

    Add Implemntation(Edit,Delete)

--- Company.G02.Mvc/Controllers/DepartmentController.cs
+++ Company.G02.Mvc/Controllers/DepartmentController.cs
@@ -44,7 +44,86 @@ namespace Company.G02.Mvc.Controllers
             }
             return View();
         }
-  
+        [HttpGet]
+        public IActionResult Details(int? id , string Actionname = "Details")
+        {
+            if (id == null || id == 0)
+            {
+                return BadRequest("Invalid Id");
+            }
+
+            var model = _departmentRepository.Get(id.Value); 
+            if (model == null)
+                return NotFound();
+            return View(model); 
+
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (id == null || id == 0)
+            //{
+            //    return BadRequest("Invalid Id");
+            //}
+
+            //var model = _departmentRepository.Get(id.Value);
+            //if (model == null)
+            //    return NotFound();
+            return Details(id,"Edit");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Department department,string action="Edit")
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != department.Id)
+                {
+                    return BadRequest("Id Mismatched");
+                }
+                var model = _departmentRepository.Update(department);
+                if (model > 0)
+                    return RedirectToAction("Index");
+            }
+
+            return View(department);
+
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            //if (id == null || id == 0)
+            //{
+            //    return BadRequest("Invalid Id");
+            //}
+
+            //var model = _departmentRepository.Get(id.Value);
+            //if (model == null)
+            //    return NotFound();
+            return Details(id,"Delete");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != department.Id)
+                {
+                    return BadRequest("Id Mismatched");
+                }
+                var model = _departmentRepository.Delete(department);
+                if (model > 0)
+                    return RedirectToAction("Index");
+            }
+
+            return Edit(id,department, "Delete");
+
+
+        }
+           
 
     }
-}
+}          
+
